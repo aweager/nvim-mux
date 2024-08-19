9c3e98a5fea817c583e8fb1b48c3bf85817573f2
Alex Eager <156651956+aweager@users.noreply.github.com>
2024-08-19 00:27:19 -0700

    Delete old impl

--- shell-hook.sh
+++ shell-hook.sh
@@ -1,6 +1,7 @@
 # Source (.) this file to set $MUX_LOCATION
 
 # TODO: find a way to set this from neovim so it applies to non-shell panes
+# TODO: wait for the mux server to be running
 if [ "$MUX_TYPE" = "nvim" ] && [ -z "$MUX_LOCATION" ]; then
     MUX_LOCATION="pid:$$"
     export MUX_LOCATION
